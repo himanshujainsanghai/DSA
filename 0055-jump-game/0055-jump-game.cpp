aa365bd0bcde5b5e16e84a6e0be28e42f3680ba7
Himanshu jain <147741865+imhimanshujainsinghai@users.noreply.github.com>
2024-07-06 11:05:50 +0530

    Time: 38 ms (85.27%), Space: 50.9 MB (51.18%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,12 +3,20 @@ public:
     bool canJump(vector<int>& nums) {
         if (nums.size() <= 1) return true;
 
-        int jumpsNeeded = 1; 
+        // int jumpsNeeded = 1; 
 
-        for (int i = nums.size() - 2; i >= 0; --i) {
-            if (nums[i] >= jumpsNeeded)  jumpsNeeded = 1; 
-            else  jumpsNeeded++; 
+        // for (int i = nums.size() - 2; i >= 0; --i) {
+        //     if (nums[i] >= jumpsNeeded)  jumpsNeeded = 1; 
+        //     else  jumpsNeeded++; 
+        // }
+        // return nums[0] >= jumpsNeeded; 
+        int maxIndex =0  ;
+
+        for(int i =0 ; i<nums.size() ; i++){
+            if(i > maxIndex) return false;
+            maxIndex = max(maxIndex , i+nums[i]);
+            if(maxIndex > nums.size()) return true;
         }
-        return nums[0] >= jumpsNeeded; 
+        return true;
     }
 };
