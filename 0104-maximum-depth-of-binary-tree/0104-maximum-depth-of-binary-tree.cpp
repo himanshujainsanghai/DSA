e00f7e3761210b5c46a4d4556b02410ec82a7c0d
Himanshu jain <147741865+imhimanshujainsinghai@users.noreply.github.com>
2024-07-12 01:02:50 +0530

    Time: 5 ms (63.11%), Space: 17.8 MB (11.59%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -10,38 +10,46 @@
  * right(right) {}
  * };
  */
-// class Solution {
-// public:
-//     int maxDepth(TreeNode* root) {
-//         if(root == NULL) return 0;
-
-//         int lh = maxDepth(root->left);
-//         int rh = maxDepth(root->right);
-
-//         return 1+max(lh,rh);
-
-//     }
-// };
-
 class Solution {
 public:
-    void solve(TreeNode* root, int height, int& maxheight) {
-        if (root == nullptr)
-            return;
-        if (root->right == nullptr && root->left == nullptr) {
-            maxheight = max(maxheight, height);
-        }
-        solve(root->left, height + 1, maxheight);
-        solve(root->right, height + 1, maxheight);
-    }
-
-public:
     int maxDepth(TreeNode* root) {
-        if (root == nullptr)
+        if (root == NULL)
             return 0;
-        int height = 1;
-        int maxheight = 0;
-        solve(root, height, maxheight);
-        return maxheight;
+
+        int lh =0 , rh =0;    
+
+        if (root->left != NULL) {
+            lh = maxDepth(root->left);
+
+        }
+        if (root->right != NULL) {
+
+            rh = maxDepth(root->right);
+        }
+
+        return 1 + max(lh, rh);
     }
 };
+
+// class Solution {
+// public:
+//     void solve(TreeNode* root, int height, int& maxheight) {
+//         if (root == nullptr)
+//             return;
+//         if (root->right == nullptr && root->left == nullptr) {
+//             maxheight = max(maxheight, height);
+//         }
+//         solve(root->left, height + 1, maxheight);
+//         solve(root->right, height + 1, maxheight);
+//     }
+
+// public:
+//     int maxDepth(TreeNode* root) {
+//         if (root == nullptr)
+//             return 0;
+//         int height = 1;
+//         int maxheight = 0;
+//         solve(root, height, maxheight);
+//         return maxheight;
+//     }
+// };
