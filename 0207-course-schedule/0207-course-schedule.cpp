ec939cfbf3034356eda1093718cea387805dccfe
Himanshu jain <147741865+himanshujainsanghai@users.noreply.github.com>
2024-07-30 12:43:17 +0530

    Time: 7 ms (98.88%), Space: 17.3 MB (64.32%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -4,38 +4,36 @@ using namespace std;
 
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        vector<int> indeg(numCourses, 0);
+    bool canFinish(int numCourses, vector<vector<int>>& pr) {
+        vector<int> adj[numCourses];
+        int n = pr.size();
+        vector<int> in(numCourses, 0);
 
-        // Build the adjacency list and calculate indegree of each vertex
-        for (const auto& pair : prerequisites) {
-            adj[pair[1]].push_back(pair[0]);
-            indeg[pair[0]]++;
+        // Build the adjacency list and in-degree array
+        for (auto i : pr) {
+            adj[i[1]].push_back(i[0]);
+            in[i[0]]++;
         }
 
+        int cnt = 0;
         queue<int> q;
-        // Add all vertices with indegree 0 to the queue
+        // Add all courses with indegree 0 to the queue
         for (int i = 0; i < numCourses; i++) {
-            if (indeg[i] == 0) {
-                q.push(i);
-            }
+            if (in[i] == 0) q.push(i);
         }
 
-        int count = 0; // Number of courses that can be completed
+        // Process the courses
         while (!q.empty()) {
-            int node = q.front();
+            int fr = q.front();
             q.pop();
-            count++;
-            for (int neighbor : adj[node]) {
-                indeg[neighbor]--;
-                if (indeg[neighbor] == 0) {
-                    q.push(neighbor);
-                }
+            cnt++;
+            for (auto it : adj[fr]) {
+                in[it]--;
+                if (in[it] == 0) q.push(it);
             }
         }
 
         // If we can complete all courses, the count should be equal to numCourses
-        return count == numCourses;
+        return cnt == numCourses;
     }
 };
