34143ad34ac364b0107c0a867f8de357fe9dbeb0
Himanshu jain <147741865+himanshujainsanghai@users.noreply.github.com>
2024-08-21 18:47:04 +0530

    Time: 2 ms (39.6%), Space: 9.5 MB (44.04%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -15,17 +15,32 @@ public:
         // return func(n-1 , nums , dp);
 
         // tabulation
-        vector<int> dp(n+1 , -1);
-        dp[0]= nums[0];
+        // vector<int> dp(n+1 , -1);
+        // dp[0]= nums[0];
 
+        // for(int i = 1 ; i<n ; i++){
+
+        //     int pick = nums[i];
+        //     if(i>1) pick += dp[i-2];
+        //     int notpick = dp[i-1];
+
+        //     dp[i] = max(pick , notpick);
+        // }
+        // return dp[n-1];
+
+        // further space optimizations
+        int prev = nums[0];
+        int prev2 = 0;
         for(int i = 1 ; i<n ; i++){
 
             int pick = nums[i];
-            if(i>1) pick += dp[i-2];
-            int notpick = dp[i-1];
+            if(i>1) pick += prev2;
+            int notpick = prev;
 
-            dp[i] = max(pick , notpick);
+            int curr = max(pick , notpick);
+            prev2 = prev;
+            prev = curr;
         }
-        return dp[n-1];
+        return prev;
     }
 };
