5cc702c8288859e41cb43f0903c177bde5a93bc5
Himanshu jain <147741865+himanshujainsanghai@users.noreply.github.com>
2024-09-15 15:00:08 +0530

    Time: 4 ms (96.75%), Space: 11.7 MB (82.1%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+        // vector<vector<int>> dp(n+1 , vector<int>(m+1 , 0));
+        // return func(n, m  , s1 , s2 , dp);
+        vector<int> prev(m + 1, 0), curr(m + 1, 0);
+        for (int ind2 = 0; ind2 <= m; ind2++)
+            prev[ind2] = 0;
+        // for(int ind1 = 0 ; ind1<= n ; ind1++) dp[ind1][0] = 0;
+        for (int ind1 = 1; ind1 <= n; ind1++) {
+            for (int ind2 = 1; ind2 <= m; ind2++) {
+                if (s1[ind1 - 1] == s2[ind2 - 1])
+                    curr[ind2] = 1 + prev[ind2 - 1];
+                else
+                    curr[ind2] = max(prev[ind2], curr[ind2 - 1]);
+            }
+            prev = curr;
+        }
+        return prev[m];
+    }
+    int minDistance(string word1, string word2) {
+        int lcs = longestCommonSubsequence(word1 , word2);
+        return word1.size() + word2.size() - 2*lcs;
+        
+    }
+};
