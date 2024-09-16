d570088445afa95a92067b88705495e335fa9103
Himanshu jain <147741865+himanshujainsanghai@users.noreply.github.com>
2024-09-16 17:38:55 +0530

    Time: 8 ms (94%), Space: 8.1 MB (87.88%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -22,13 +22,13 @@ public:
         prev[0] = curr[0] = 1;
         // for(int j =1 ; j<=m ;j++) dp[0][j] = 0;
         for(int i = 1 ; i<=n ; i++){
-            for(int j = 1; j<=m ; j++){
+            for(int j = m ; j>=1 ; j--){
                 if(s[i-1] == t[j-1]){
-                    curr[j] = prev[j-1] + prev[j];
+                    prev[j] += prev[j-1];
                 }
-                else curr[j] = prev[j];
+                // else curr[j] = prev[j];
             }
-            prev = curr;
+            // prev = curr;
         }
         return  (int)prev[m];
         
