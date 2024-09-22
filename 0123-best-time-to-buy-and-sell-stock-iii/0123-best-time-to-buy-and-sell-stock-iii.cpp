cfbe2495d081e6c628ae2deb400dc185ba9758df
Himanshu jain <147741865+himanshujainsanghai@users.noreply.github.com>
2024-09-22 20:45:47 +0530

    Time: 129 ms (77.92%), Space: 78.3 MB (87.68%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+private:
+    int func(int ind, int buy, int cap, int n, vector<int>& p,
+             vector<vector<vector<int>>>& dp) {
+        if (cap == 0 || ind == n)
+            return 0;
+        if (dp[ind][buy][cap] != -1)
+            return dp[ind][buy][cap];
+        if (buy) {
+            return dp[ind][buy][cap] =
+                       max(-p[ind] + func(ind + 1, 0, cap, n, p, dp),
+                           func(ind + 1, 1, cap, n, p, dp));
+        }
+        return dp[ind][buy][cap] =
+                   max(p[ind] + func(ind + 1, 1, cap - 1, n, p, dp),
+                       func(ind + 1, 0, cap, n, p, dp));
+    }
+
+public:
+    int maxProfit(vector<int>& p) {
+        int n = p.size();
+        // vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+        // return func(0 , 1 , 2 , n , p , dp);
+        vector<vector<int>> after(2, vector<int>(3, 0));
+        vector<vector<int>> curr(2, vector<int>(3, 0));
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= 2; cap++) {
+                    if (buy) {
+                        curr[buy][cap] = max(-p[ind] + after[0][cap],
+                                                after[1][cap]);
+                    } else {
+                        curr[buy][cap] =
+                            max(p[ind] + after[1][cap - 1],
+                                after[0][cap]);
+                    }
+                }
+            }
+            after = curr;
+        }
+        return after[1][2];
+    }
+};
