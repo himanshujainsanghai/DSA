f4dc6bdaf27308cfeccc174df19c1209cf069c49
Himanshu jain <147741865+himanshujainsanghai@users.noreply.github.com>
2024-09-24 14:08:21 +0530

    Time: 0 ms (100%), Space: 12.5 MB (92.89%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,25 +1,41 @@
 class Solution {
-    void func(int ind , int target , vector<int>& candi , vector<vector<int>>& ans , vector<int>& ds , int n){
+    void func(int ind, int target, vector<int>& candi, vector<vector<int>>& ans,
+              vector<int>& ds, int n) {
 
-        if(ind == n ){
-            if(target == 0) ans.push_back(ds);
+        if (ind == n) {
+            if (target == 0)
+                ans.push_back(ds);
             return;
         }
 
-        if(candi[ind] <= target){
+        if (candi[ind] <= target) {
             ds.push_back(candi[ind]);
-            func(ind , target-candi[ind] , candi , ans , ds , n);
+            func(ind, target - candi[ind], candi, ans, ds, n);
             ds.pop_back();
         }
-        func(ind +1 , target , candi , ans , ds , n);
+        func(ind + 1, target, candi, ans, ds, n);
     }
+    void solve(int ind, int target, vector<int>& candi,
+               vector<vector<int>>& ans, vector<int>& ds, int n) {
+        if (target == 0) {
+            ans.push_back(ds);
+            return;
+        }
+        for (int i = ind ; i < n; i++) {
+            if (candi[i] <= target) {
+                ds.push_back(candi[i]);
+                solve(i, target - candi[i], candi, ans, ds, n);
+                ds.pop_back();
+            }
+        }
+    }
+
 public:
     vector<vector<int>> combinationSum(vector<int>& candi, int target) {
         int n = candi.size();
         vector<vector<int>> ans;
         vector<int> ds;
-        func(0 , target , candi , ans , ds , n);
+        solve(0, target, candi, ans, ds, n);
         return ans;
-        
     }
 };
