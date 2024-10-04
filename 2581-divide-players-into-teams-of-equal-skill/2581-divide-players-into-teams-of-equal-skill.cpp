2f616ccc5a9b2d0f6543bd27a25c8c72a26a9fff
Himanshu jain <147741865+himanshujainsanghai@users.noreply.github.com>
2024-10-04 20:08:24 +0530

    Time: 41 ms (99.6%), Space: 55.8 MB (98.4%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
@@ -1,20 +1,44 @@
 class Solution {
+    private:
+    long long func(vector<int>& skill){
+        int n = skill.size();
+        vector<int> vec(1001 , 0);
+        int sum = 0;
+        for(int i =0 ; i<n ; i++){
+            sum += skill[i];
+            vec[skill[i]]++;
+
+        }
+        int teams = n/2;
+        if(sum % teams != 0) return -1;
+        int target = sum/teams;
+        long long chem =0 ;
+        for(int i =0 ; i<n ; i++){
+            int remskill = target - skill[i];
+            if(vec[remskill] <= 0) return -1;
+            chem += (long long)(skill[i]) * (long long)(remskill);
+            vec[remskill] -=1;
+        }
+        return chem/2;
+
+    }
 public:
     long long dividePlayers(vector<int>& skill) {
-        sort(skill.begin() , skill.end());
-        int n = skill.size();
-        if(n==2) return skill[0]*skill[1];
-        int i =0 , j = n-1;
-        int s = skill[i]+skill[j];
-        long long chem = 0;
-        while(i<j){
-            int sum = skill[i]+skill[j];
-            if(sum != s) return -1;
-            chem += (long long)(skill[i]) * (long long)(skill[j]);
-            i++;
-            j--;
-        }
-        return chem;
+        // sort(skill.begin() , skill.end());
+        // int n = skill.size();
+        // if(n==2) return skill[0]*skill[1];
+        // int i =0 , j = n-1;
+        // int s = skill[i]+skill[j];
+        // long long chem = 0;
+        // while(i<j){
+        //     int sum = skill[i]+skill[j];
+        //     if(sum != s) return -1;
+        //     chem += (long long)(skill[i]) * (long long)(skill[j]);
+        //     i++;
+        //     j--;
+        // }
+        // return chem;
+        return func(skill);
         
     }
 };
