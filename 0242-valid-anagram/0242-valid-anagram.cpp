58b5cdfd11c4e08891e947cce87ed6e385416974
Himanshu jain <147741865+himanshujainsanghai@users.noreply.github.com>
2024-10-12 17:38:51 +0530

    Time: 5 ms (82.06%), Space: 8.7 MB (93.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -6,11 +6,29 @@ public:
         cout.tie(NULL);
         if (s.size() != t.size())
             return false;
-        unordered_map<int, int> mpp1, mpp2;
-        for (int i = 0; i < s.size(); i++) {
-            mpp1[s[i] - 'a']++;
-            mpp2[t[i] - 'a']++;
+
+        // Approach1 : TC=O(N) while SC=O(2*sizeOfString); 
+        // unordered_map<int, int> mpp1, mpp2;
+        // for (int i = 0; i < s.size(); i++) {
+        //     mpp1[s[i] - 'a']++;
+        //     mpp2[t[i] - 'a']++;
+        // }
+        // return mpp1 == mpp2;
+        
+        // Approach 2 TC=O(2*nlogn) , SC=O(1)
+        // sort(s.begin() , s.end());
+        // sort(t.begin() , t.end());
+        // return s==t;
+
+        // Approach 3
+        vector<int> frq(26,0);
+        for(int i=0 ; i<s.size() ; i++){
+            frq[s[i]-'a']++;
+            frq[t[i]-'a']--;
         }
-        return mpp1 == mpp2;
+        for(auto &count:frq){
+            if(count != 0) return false;
+        }
+        return true;
     }
 };
