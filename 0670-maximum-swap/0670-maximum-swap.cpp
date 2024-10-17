e9bd63b41982b042d4ec4b711615253c399f28dd
Himanshu jain <147741865+himanshujainsanghai@users.noreply.github.com>
2024-10-17 11:32:19 +0530

    Time: 0 ms (100%), Space: 7.6 MB (70.34%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     int maximumSwap(int num) {
+        ios::sync_with_stdio(false); // Disable synchronization with C I/O
+        cin.tie(NULL);               // Untie cin from cout
+        cout.tie(NULL);        
         if (num == 0) {
             return 0;
         }
         vector<int> vis;
-        while (num > 0) {
-            vis.push_back(num % 10);
-            num /= 10;
+        int nums = abs(num);
+        while (nums > 0) {
+            vis.push_back(nums % 10);
+            nums /= 10;
         }
         reverse(vis.begin(), vis.end());
-        int maxi = 0;
-        int maxInd = -1, swapi = -1, swapj = -1;
-        for (int i = vis.size() - 1; i >= 0; i--) {
+        int n=vis.size();
+        int maxi = vis[n-1];
+        int maxInd = n-1, swapi = -1, swapj = -1;
+        for (int i = n-2; i >= 0; i--) {
             if (vis[i] > maxi) {
                 maxi = vis[i];
                 maxInd = i;
@@ -22,11 +27,13 @@ public:
                 swapj = maxInd;
             }
         }
-        if (swapi != -1) {
-            swap(vis[swapi], vis[swapj]);
+        if (swapi == -1) {
+            return num;
+
         }
+        swap(vis[swapi], vis[swapj]);
         int ans=0;
-        for(int i=0 ; i<vis.size() ; i++){
+        for(int i=0 ; i<n ; i++){
             ans = ans*10 + vis[i];
         }
 
