85dca3b577957f99192611933a217fb2f19ecf92
Himanshu jain <147741865+himanshujainsanghai@users.noreply.github.com>
2024-10-27 14:04:33 +0530

    Time: 7 ms (51.85%), Space: 29 MB (56.4%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -1,53 +1,69 @@
 class Solution {
-public:
-int m;
-int n;
+private:
+    // int m;
+    // int n;
 
-int solve(int i ,int j , vector<vector<int>>& grid , vector<vector<int>>& dp){
-    if(i>=m || j>=n){
-        return 0;
-    }
-    if(grid[i][j] != 1){
-        return 0;
-    }
-    if(dp[i][j] != -1){
-        return dp[i][j];
-    }
-    int hori = solve(i ,j+1 , grid , dp);
-    int daig = solve(i+1 ,j+1 , grid , dp);
-    int below = solve(i+1 ,j , grid ,dp);
-
-    return dp[i][j] =  1 + min({hori , daig , below});
-}
-    int countSquares(vector<vector<int>>& grid) {
-      m = grid.size();
-      n = grid[0].size();  
-
-      int result =0;
-    vector<vector<int>> dp(m+1 , vector<int>(n+1 , 0));
-    //   for(int i=0 ; i<m ; i++){
-    //     for(int j=0 ; j<n ; j++){
-    //         if(grid[i][j] == 1){
-    //             result += solve(i ,j , grid , dp);
-    //         }
+    // int solve(int i, int j, vector<vector<int>>& grid,
+    //           vector<vector<int>>& dp) {
+    //     if (i >= m || j >= n) {
+    //         return 0;
     //     }
-    //   }
+    //     if (grid[i][j] != 1) {
+    //         return 0;
+    //     }
+    //     if (dp[i][j] != -1) {
+    //         return dp[i][j];
+    //     }
+    //     int hori = solve(i, j + 1, grid, dp);
+    //     int daig = solve(i + 1, j + 1, grid, dp);
+    //     int below = solve(i + 1, j, grid, dp);
 
-    // TABULATION
+    //     return dp[i][j] = 1 + min({hori, daig, below});
+    // }
 
-    for(int i=0 ; i<m ; i++){
-        for(int j=0 ; j<n ; j++){
-            if(i==0 || j==0){
-                dp[i][j] = grid[i][j];
-            }
-            else if(grid[i][j] == 1){
-                dp[i][j] = 1 + min({dp[i-1][j-1] , dp[i-1][j] , dp[i][j-1]});
-            }
+public:
+    int countSquares(vector<vector<int>>& grid) {
+        ios::sync_with_stdio(false); // Disable synchronization with C I/O
+        cin.tie(NULL);               // Untie cin from cout
+        cout.tie(NULL);
 
-            result += dp[i][j];
+
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int result = 0;
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        //   for(int i=0 ; i<m ; i++){
+        //     for(int j=0 ; j<n ; j++){
+        //         if(grid[i][j] == 1){
+        //             result += solve(i ,j , grid , dp);
+        //         }
+        //     }
+        //   }
+
+        // TABULATION
+
+        for(int i=0 ; i<m ; i++){
+            dp[i][0] = grid[i][0];
+            result += dp[i][0];
         }
-    }
 
-      return result;
+        for(int j=1 ; j<n ; j++){
+            dp[0][j] = grid[0][j];
+            result += dp[0][j];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    dp[i][j] =
+                        1 + min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]});
+                }
+
+                result += dp[i][j];
+            }
+        }
+
+        return result;
     }
 };
