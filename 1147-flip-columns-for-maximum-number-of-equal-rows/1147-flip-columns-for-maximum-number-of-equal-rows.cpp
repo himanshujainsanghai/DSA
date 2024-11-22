cb5fc0a25635076fc9c2e441a03bb187c9c7f69c
Himanshu jain <147741865+himanshujainsanghai@users.noreply.github.com>
2024-11-22 13:52:17 +0530

    Time: 19 ms (78.26%), Space: 74.5 MB (71.64%) - LeetHub

--- 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.cpp
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -1,25 +1,50 @@
 class Solution {
 public:
     int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
-        int m = matrix.size();
+        // int m = matrix.size();
         int n = matrix[0].size();
         int maxrows = 0;
 
-        for(auto &vec:matrix){
-            vector<int> inverted(n , 0);
-            for(int i=0 ; i<n ; i++){
-                if(vec[i] == 0) inverted[i] = 1;
+        // for(auto &vec:matrix){
+        //     vector<int> inverted(n , 0);
+        //     for(int i=0 ; i<n ; i++){
+        //         if(vec[i] == 0) inverted[i] = 1;
+        //     }
+
+        //     int count =0;
+        //     for(auto& row:matrix){
+        //         if(row == vec || row == inverted){
+        //             count++;
+        //         }
+        //     }
+
+        //     maxrows = max(maxrows , count);
+        // }
+        // return maxrows;
+
+
+
+        // OPITMAL APPROACH
+        // tracking the nature of the rows
+
+        unordered_map<string, int> mpp;
+        for(auto& row: matrix){
+            int firstchar = row[0];
+            string curr = "";
+            for(int j=0 ; j<n ; j++){
+                curr += (row[j]==firstchar)? 's' :'c';
             }
 
-            int count =0;
-            for(auto& row:matrix){
-                if(row == vec || row == inverted){
-                    count++;
-                }
-            }
+            mpp[curr]++;
 
-            maxrows = max(maxrows , count);
+            maxrows = max(maxrows , mpp[curr]);
         }
+
+        // traversal to check maximum count within map
+        // for(auto& it:mpp){
+        //     maxrows = max(maxrows , it.second);
+        // }
+
         return maxrows;
     }
 };
