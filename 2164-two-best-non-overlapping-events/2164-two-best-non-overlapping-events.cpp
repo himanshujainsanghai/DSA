a1855b64e84e86e654701860325568b9cf9add1a
Himanshu jain <147741865+himanshujainsanghai@users.noreply.github.com>
2024-12-08 12:51:14 +0530

    Time: 143 ms (34.68%), Space: 155.3 MB (20.98%) - LeetHub

---
+++ 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+int n;
+
+
+// upper_bound
+int binarySearch(vector<vector<int>>& events , int endtime){
+    int l=0 , r= n-1 , result = n , mid =0;
+
+    while(l<=r){
+        mid = l+ (r-l)/2;
+
+        if(events[mid][0] > endtime){
+            result = mid;
+            r = mid-1;
+        }
+        else{
+            l = mid+1;
+        }
+    }
+
+    return result;
+}
+
+int solve(vector<vector<int>>& events , int i , int count , vector<vector<int>>& dp){
+    if( count ==2 || i==n){
+        return 0;
+    }
+
+    if(dp[i][count] != -1){
+        return dp[i][count];
+    }
+
+    int nextValidIndex = binarySearch(events , events[i][1]); 
+    int take = events[i][2] + solve(events , nextValidIndex ,count+1 , dp);
+    int not_take = solve(events , i+1 , count , dp);
+
+    return dp[i][count]= max(take , not_take);
+}
+    int maxTwoEvents(vector<vector<int>>& events) {
+        n = events.size();
+        int count =0;
+        sort(events.begin() , events.end());
+        vector<vector<int>> dp(n+1 , vector<int>(3  , -1));
+        return solve(events , 0 , count , dp);
+        
+    }
+};
