46e12f12f7840b964c3b53d2c5bce5a833866a58
Himanshu jain <147741865+himanshujainsanghai@users.noreply.github.com>
2024-12-20 18:42:52 +0530

    Time: 0 ms (100%), Space: 77.2 MB (65.15%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
@@ -10,49 +10,72 @@
  * };
  */
 class Solution {
-public:
-    TreeNode* reverseOddLevels(TreeNode* root) {
 
-        queue<TreeNode*> que;
-        que.push(root);
-        int level = 0;
-
-        while(!que.empty()){
-            int n = que.size();
-            vector<TreeNode*> levelnodes;
-            while(n--){
-                TreeNode* temp = que.front();
-                que.pop();
-
-                levelnodes.push_back(temp);
-                if(temp->left){
-                    que.push(temp->left);
-                }
-                if(temp->right){
-                    que.push(temp->right);
-                }
-
-            }
-
-
-            if(level%2 == 1){
-                // odd level
-
-                int i=0 , j = levelnodes.size()-1;
-                while(i<j){
-                    int value = levelnodes[i]->val;
-                    levelnodes[i]->val = levelnodes[j]->val;
-                    levelnodes[j]->val = value;
-                    i++;
-                    j--;
-                }
-
-
-            }
-            level++;
+    void solve(TreeNode* L , TreeNode* R , int level){
+        if(L == NULL || R == NULL){
+            return;
         }
 
+        if(level%2 == 1){
+            int value = L->val;
+            L->val = R->val;
+            R->val = value; 
+        }
 
+        solve(L->left , R->right , level +1);
+        solve(L->right , R->left , level+1);
+
+    } 
+public:
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        // BFS Approach
+        // queue<TreeNode*> que;
+        // que.push(root);
+        // int level = 0;
+
+        // while(!que.empty()){
+        //     int n = que.size();
+        //     vector<TreeNode*> levelnodes;
+        //     while(n--){
+        //         TreeNode* temp = que.front();
+        //         que.pop();
+
+        //         levelnodes.push_back(temp);
+        //         if(temp->left){
+        //             que.push(temp->left);
+        //         }
+        //         if(temp->right){
+        //             que.push(temp->right);
+        //         }
+
+        //     }
+
+
+        //     if(level%2 == 1){
+        //         // odd level
+
+        //         int i=0 , j = levelnodes.size()-1;
+        //         while(i<j){
+        //             int value = levelnodes[i]->val;
+        //             levelnodes[i]->val = levelnodes[j]->val;
+        //             levelnodes[j]->val = value;
+        //             i++;
+        //             j--;
+        //         }
+
+
+        //     }
+        //     level++;
+        // }
+
+
+        // return root;
+
+
+
+        // DFS Approach
+
+        solve(root->left , root->right , 1);
         return root;
         
     }
