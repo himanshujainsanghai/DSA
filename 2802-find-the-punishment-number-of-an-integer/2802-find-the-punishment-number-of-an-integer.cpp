b329c06a5cb34f498cff8b7202728299e80d4a30
Himanshu Jain <147741865+himanshujainsanghai@users.noreply.github.com>
2025-02-15 12:41:26 +0530

    Time: 51 ms (70.28%), Space: 9 MB (71.28%) - LeetHub

---
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> nums;
+    unordered_map<int, int> mpp;
+    int currSum = 0;
+    bool check(int num, int currnum, int target) {
+        if (num == 0) {
+            return currnum == target;
+        }
+
+        return check(num / 10, currnum + num % 10, target) ||
+               check(num / 100, currnum + num % 100, target) ||
+               check(num / 1000, currnum + num % 1000, target) ||
+               check(num / 10000, currnum + num % 10000, target);
+    }
+    Solution() {
+        for (int i = 1; i <= 1000; i++) {
+            int sq = i * i;
+            if (check(sq, 0, i) == true) {
+                currSum += sq;
+                mpp[i] = currSum;
+                nums.push_back(i);
+            } else
+                continue;
+        }
+    }
+
+    static Solution& getInstance() {
+        static Solution instance;
+        return instance;
+    }
+
+    int punishmentNumber(int n) {
+        if (mpp.find(n) != mpp.end()) {
+            return mpp[n];
+        }
+        auto lower = std::lower_bound(nums.begin(), nums.end(), n);
+        if (lower == nums.begin())
+            return 0; // Safe bound check
+
+        --lower;
+        return mpp[*lower];
+    }
+};
