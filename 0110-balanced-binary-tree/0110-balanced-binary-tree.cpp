60a398d5f79b81f6de6b512e054f6d9957ad8040
Himanshu Jain <147741865+himanshujainsanghai@users.noreply.github.com>
2026-01-04 18:12:46 +0530

    Time: 0 ms (100%), Space: 23 MB (83.19%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -6,25 +6,27 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int height(TreeNode* root) {
-        if (root == NULL)
-            return 0;
-        int lh = height(root->left);
-        if (lh == -1)
-            return -1;
-        int rh = height(root->right);
-        if (rh == -1)
-            return -1;
+    int func(TreeNode* node){
+        if(node == NULL) return 0;
 
-        if (abs(lh - rh) > 1)
-            return -1;
-        return 1 + max(lh, rh);
+        int lh = func(node->left);
+        int rh = func(node->right);
+
+        if(lh == -1 || rh == -1) return -1;
+
+        if(abs(rh-lh)>1) return -1;
+
+        return 1+max(lh, rh);
     }
-    bool isBalanced(TreeNode* root) { return height(root) != -1; }
+    bool isBalanced(TreeNode* root) {
+        if(func(root) == -1) return false;
+        return true;
+        
+        
+    }
 };
